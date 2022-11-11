00259256679cc45513e487b906387da0f6a2b9bc
piusobonyo-admin <piusobonyo5@gmail.com>
2022-11-11 23:05:28 +0300

    added filies

---
+++ src/main/java/com/pius/demo_Bank/DemoBankApplication.java
@@ -0,0 +1,13 @@
+package com.pius.demo_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBankApplication.class, args);
+	}
+
+}
